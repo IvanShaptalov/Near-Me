bba75b4a16b6e81cc984c4504def77e7d8be76f0
IvanShaptalov <ivan.shaptalov1@gmail.com>
2025-05-08 21:08:29 +0200

    Initial Commit

---
+++ Near Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Near Me
+//
+//  Created by PowerMac on 08.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
